513be7c7a3965eaf4a8e8aad07e429c0316a4a75
Md Sajal Mia <sajalmia381@gmail.com>
2021-09-05 23:30:03 +0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-<USERNAME>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'store-admin';
+}
