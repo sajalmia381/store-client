28c073561b6ac5c8a6a88bba568f808b0d8c5f30
Md Sajal Mia <sajalmia381@gmail.com>
2021-09-06 11:22:20 +0600

    login page implement

---
+++ src/app/auth/logout/logout.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  template: ``
+})
+export class LogoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
