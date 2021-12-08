d38c2bba2879a6ccb9a86cbc8c5bc8818be4330b
Md Sajal Mia <sajalmia381@gmail.com>
2021-12-08 23:02:31 +0600

    google analytic added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { environment } from '../environments/environment';
@@ -15,6 +14,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClintInterceptor } from '@shared/services/http.interceptor';
 import { DefaultLayoutModule } from './default-layout/default-layout.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,6 +25,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     StoreRouterConnectingModule.forRoot({ serializer: RouterSerializer }),
+    NgxGoogleAnalyticsModule.forRoot(environment.GA),
+    NgxGoogleAnalyticsRouterModule.forRoot({exclude: ['/admin/*'] }),
     DefaultLayoutModule,
     HttpClientModule,
     MatSnackBarModule
