71c02bcc021a27be0598ecf66e46064ba5606324
Md Sajal Mia <sajalmia381@gmail.com>
2021-12-22 18:06:42 +0600

    update13 initial

--- src/app/@admin/media/media.module.ts
+++ src/app/@admin/media/media.module.ts
@@ -8,8 +8,7 @@ import { AddImageComponent } from './add-image/add-image.component';
 import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [ImageListComponent, AddImageComponent],
-  imports: [CommonModule, MediaRoutingModule, SharedModule, MatDialogModule],
-  entryComponents: [AddImageComponent]
+    declarations: [ImageListComponent, AddImageComponent],
+    imports: [CommonModule, MediaRoutingModule, SharedModule, MatDialogModule]
 })
 export class MediaModule {}
