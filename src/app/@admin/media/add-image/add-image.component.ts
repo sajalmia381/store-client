e1df3d12aa21bd0414fe546e9ccbe7e670e6861f
Md. Sajal Mia <sajalmia381@gmail.com>
2023-11-04 10:44:44 +0600

    upgrade to 15

--- src/app/@admin/media/add-image/add-image.component.ts
+++ src/app/@admin/media/add-image/add-image.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
 import { ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { addImage, addImageSuccess } from '../state/media.actions';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
 
 import { DeleteConformationComponent } from './components/delete-conformation/delete-conformation.component';
 import { FooterComponent } from './components/footer/footer.component';
