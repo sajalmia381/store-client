dbed0fb6235c49f2e04b7ab4d2e6c0589862548d
Md. Sajal Mia <sajalmia381@gmail.com>
2023-11-30 23:30:20 +0600

    reactor: optimization with angular 17

--- src/app/@admin/user/user-list/user-list.component.ts
+++ src/app/@admin/user/user-list/user-list.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { DeleteConformationComponent } from '@shared/components/delete-conformation/delete-conformation.component';
 import { Observable } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
 import { deleteUser, loadUsers } from '../state/user.actions';
 import { getUsers, isLoaded } from '../state/user.selectors';
 import { UserState } from '../state/user.state';
 import { User } from '../user';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit, OnDestroy {
-  isAlive: boolean = true;
+export class UserListComponent implements OnInit {
 
+  user$!: Observable<User[]>;
   isLoaded$!: Observable<boolean>;
   loading: boolean = false;
 
@@ -32,21 +32,20 @@ export class UserListComponent implements OnInit, OnDestroy {
     'action'
   ];
   dataSource!: User[];
-  constructor(private store: Store<UserState>, private dialog: MatDialog) {}
+
+  constructor(private store: Store<UserState>, private dialog: MatDialog) {
+    this.user$ = this.store.select(getUsers).pipe(takeUntilDestroyed());
+    this.isLoaded$ = this.store.select(isLoaded).pipe(takeUntilDestroyed());
+  }
 
   ngOnInit(): void {
-    this.store
-      .select(getUsers)
-      .pipe(takeWhile(() => this.isAlive))
+    this.user$
       .subscribe(data => {
         this.dataSource = data;
       });
-    this.isLoaded$ = this.store.select(isLoaded);
     this.store.dispatch(loadUsers());
   }
-  ngOnDestroy(): void {
-    this.isAlive = false;
-  }
+  
   onDelete(user: User): void {
     const dialogRef = this.dialog.open(DeleteConformationComponent, {
       data: {
