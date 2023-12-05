3615f4fb651bd8ba833e87576f537f3ad89721fb
Md. Sajal Mia <sajalmia381@gmail.com>
2023-12-05 19:07:21 +0600

    feat(cart): cart CURD doc implement

--- src/app/v0/doc/docDate.ts
+++ src/app/v0/doc/docDate.ts
@@ -66,6 +66,132 @@ export default {
         .then(json => console.log(json))`
     }
   ],
+  category: [
+    {
+      name: 'Get Categories',
+      code: `fetch('${apiBaseUrl}/categories')
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Get Single Category',
+      code: `fetch('${apiBaseUrl}/categories/mens-cloths')
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Add New Category',
+      code: `fetch('${apiBaseUrl}/categories', 
+        {
+            method: 'POST',
+            body: JSON.stringify({
+              name: 'mens-cloths',
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Update Category',
+      code: `fetch('${apiBaseUrl}/categories/mens-cloths',
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            name: 'Mens Fashion',
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Delete Category',
+      code: `fetch('${apiBaseUrl}/categories/mens-cloths',
+        {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    }
+  ],
+  cart: [
+    {
+      name: 'Get Cart',
+      code: `fetch('${apiBaseUrl}/carts')
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Get Single Cart',
+      code: `fetch('${apiBaseUrl}/carts/6569bdf94e2a60e28710d724')
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Add New Cart',
+      code: `fetch('${apiBaseUrl}/carts', 
+        {
+            method: 'POST',
+            body: JSON.stringify({
+              "userId": "612e48bf345dcc333ac6cb28",
+              "products": [
+                {
+                  "productId": "61ab43350f34753bcedfa7aa",
+                  "quantity": 5
+                },
+                {
+                  "productId": "61ab434b0f34753bcedfa7ae",
+                  "quantity": 7
+                } 
+              ]
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Update Cart',
+      code: `fetch('${apiBaseUrl}/carts/6569bdf94e2a60e28710d724',
+        {
+            method: 'PUT',
+            body: JSON.stringify({
+              "products": [
+                {
+                  "productId": "61ab43350f34753bcedfa7aa",
+                  "quantity": 15
+                },
+                {
+                  "productId": "61ab434b0f34753bcedfa7ae",
+                  "quantity": 20
+                } 
+              ]
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    },
+    {
+      name: 'Delete Cart',
+      code: `fetch('${apiBaseUrl}/carts/6569bdf94e2a60e28710d724',
+        {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))`
+    }
+  ],
+
   user: [
     {
       name: 'Get User',
@@ -119,59 +245,6 @@ export default {
     {
       name: 'Delete User',
       code: `fetch('${apiBaseUrl}/users/612e4851345dcc333ac6cb24',
-        {
-            method: 'DELETE',
-        })
-        .then(response => response.json())
-        .then(json => console.log(json))`
-    }
-  ],
-  category: [
-    {
-      name: 'Get Categories',
-      code: `fetch('${apiBaseUrl}/categories')
-        .then(response => response.json())
-        .then(json => console.log(json))`
-    },
-    {
-      name: 'Get Single Category',
-      code: `fetch('${apiBaseUrl}/categories/mens-cloths')
-        .then(response => response.json())
-        .then(json => console.log(json))`
-    },
-    {
-      name: 'Add New Category',
-      code: `fetch('${apiBaseUrl}/categories', 
-        {
-            method: 'POST',
-            body: JSON.stringify({
-              name: 'mens-cloths',
-            }),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-        .then(response => response.json())
-        .then(json => console.log(json))`
-    },
-    {
-      name: 'Update Category',
-      code: `fetch('${apiBaseUrl}/categories/mens-cloths',
-        {
-          method: 'PUT',
-          body: JSON.stringify({
-            name: 'Mens Fashion',
-          }),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        })
-        .then(response => response.json())
-        .then(json => console.log(json))`
-    },
-    {
-      name: 'Delete Category',
-      code: `fetch('${apiBaseUrl}/categories/mens-cloths',
         {
             method: 'DELETE',
         })
@@ -254,5 +327,5 @@ export default {
         .then(response => response.json())
         .then(json => console.log(json))`
     }
-  ],
+  ]
 };
