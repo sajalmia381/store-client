7049669c02394eef847150bacbed3a61dae6de14
Md. Sajal Mia <sajalmia381@gmail.com>
2023-12-21 00:16:32 +0600

    home hero improvemenet

--- src/app/v0/home/home.component.ts
+++ src/app/v0/home/home.component.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '@shared/shared.module';
 import { AsPrismModule } from 'as-prism';
 
 import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-json';
 
 @Component({
   selector: 'app-home',
@@ -18,12 +19,37 @@ export class HomeComponent implements OnInit {
   apiBaseUrl: string = environment.apiBaseUrl;
   adminRoutePrefix = '/admin'
   productEndpoint: string = '/products/running-sneaker';
-  code: string = `
+  readonly code = `
 fetch('${environment.apiBaseUrl + this.productEndpoint}')
 	.then(response => response.json())
 	.then(json => console.log(json))`;
   productRes: any;
   isProductLoading!: boolean;
+  readonly heroShowcase = `
+  {
+    "status": 200,
+    "data": {
+        "_id": "61ab43350f34753bcedfa7aa",
+        "title": "men casual shoes sports running sneakers",
+        "price": 100,
+        "category": {
+            "_id": "61ab1d0e4a0fef3f27dc664d",
+            "name": "bags & shoes",
+            "slug": "bags-and-shoes"
+        },
+        "description": null,
+        "createdBy": {
+            "role": "ROLE_CUSTOMER",
+            "_id": "612e48fb345dcc333ac6cb2e",
+            "name": "Noah Ali"
+        },
+        "createdAt": "2021-12-04T10:30:13.299Z",
+        "updatedAt": "2021-12-04T10:30:13.299Z",
+        "slug": "men-casual-shoes-sports-running-sneakers"
+    },
+    "message": "Success! Product found"
+  }
+  `
 
   constructor(private http: HttpService) { }
 
