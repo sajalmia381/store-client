e23e8a4e1ebef3bb8672441df356936271156359
Md. Sajal Mia <sajalmia381@gmail.com>
2023-12-23 01:27:17 +0600

    adnsene id add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   apiBaseUrl: domain,
   baseUrl: domain,
   v1BaseUrl: domain + "/v1",
-  GA: 'G-FZ9HXX4GYB'
+  GA: 'G-FZ9HXX4GYB',
+  adsenseClientId: "ca-pub-6922458926245751"
 };
