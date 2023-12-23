b02e36260499b1b5da78f44520ae1f2844293ac9
Md. Sajal Mia <sajalmia381@gmail.com>
2023-12-23 22:49:22 +0600

    resource route separated

---
+++ src/app/v0/doc/containers/cart-doc/cart-doc.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CartDocComponent } from './cart-doc.component';
+
+describe('CartDocComponent', () => {
+  let component: CartDocComponent;
+  let fixture: ComponentFixture<CartDocComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CartDocComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CartDocComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/v0/doc/doc-routing.module.ts
+++ src/app/v0/doc/doc-routing.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DocComponent } from './doc.component';
+import { docResourceResolver } from './common/resolvers/doc-resource.resolver';
+
+const resourceRoute: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./containers/intro-doc/intro-doc.component').then(m => m.IntroDocComponent)
+  },
+  {
+    path: 'product',
+    loadComponent: () => import('./containers/product-doc/product-doc.component').then(m => m.ProductDocComponent),
+  },
+  {
+    path: 'category',
+    loadComponent: () => import('./containers/category-doc/category-doc.component').then(m => m.CategoryDocComponent),
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./containers/cart-doc/cart-doc.component').then(m => m.CartDocComponent),
+  },
+  {
+    path: 'user',
+    loadComponent: () => import('./containers/user-doc/user-doc.component').then(m => m.UserDocComponent),
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./containers/auth-doc/auth-doc.component').then(m => m.AuthDocComponent),
+  },
+  {
+    path: 'todo',
+    loadComponent: () => import('./containers/todo-doc/todo-doc.component').then(m => m.TodoDocComponent),
+  }
+];
 
 const routes: Routes = [
   {
     path: '',
-    component: DocComponent
+    component: DocComponent,
+    children: resourceRoute
   }
 ];
 
