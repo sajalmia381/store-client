c5f68d57c732ac6362f607fa0ab90405d866ac97
Md Sajal Mia <sajalmia381@gmail.com>
2025-04-05 14:54:11 +0600

    fix

--- src/app/v0/doc/containers/intro-doc/intro-doc.component.ts
+++ src/app/v0/doc/containers/intro-doc/intro-doc.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { productDoc, categoryDoc, cartDoc, userDoc, authDoc, todoDoc } from '../../common/data';
-import { KeyValuePipe, NgClass, NgTemplateOutlet, TitleCasePipe } from '@angular/common';
+import { KeyValuePipe, NgTemplateOutlet, TitleCasePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { AsPrismModule } from 'as-prism';
 
 @Component({
     selector: 'app-intro-doc',
-    imports: [RouterLink, NgTemplateOutlet, TitleCasePipe, KeyValuePipe, NgClass, MatIconModule, AsPrismModule],
+    imports: [RouterLink, NgTemplateOutlet, TitleCasePipe, KeyValuePipe, MatIconModule, AsPrismModule],
     templateUrl: './intro-doc.component.html',
     styleUrl: './intro-doc.component.scss'
 })
